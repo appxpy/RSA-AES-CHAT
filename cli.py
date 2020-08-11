55e542f6ca572126fc47d6868021c0f6e88241f0
APP x PY <mayseyfaynpokemon@gmail.com>
2020-08-11 23:51:19 +0300

    upd

--- cli.py
+++ cli.py
@@ -1,84 +1,126 @@
-try:
-    import socket
-    import time
-    import threading
-    import sys
-    from cryptography.fernet import Fernet
-except:
-	print('Installing packages!')
-	import subprocess
-	import sys
-	packagelist = ['cryptography']
-	for package in packagelist:
-		subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-finally:
-	import socket
-	import time
-	import threading
-	import sys
-	from cryptography.fernet import Fernet
 
+import socket
+import time
+import threading
+import sys
+import hashlib
+import rsa
+import json
+from requests import get
+from requests import get
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 0
-server = ('192.168.14.55', 8080)
+ip = get('https://api.ipify.org').text
+
+global KEYS
+KEYS = []
+HOST = socket.gethostbyname("") 
+PORT = 8761
+server = ('127.0.0.1', 8760)
 
 
 join = False
 shutdown = False
 key_recieved = False
+authorized = False
 
 def recieve(name, socket):
 	while shutdown == False:
 		try:
 			while True:
-				data, addr = socket.recvfrom(1024)
+				data, addr = socket.recvfrom(2048)
 		
 				data = f.decrypt(data).decode('utf-8')
-		
-				if data != '---KEYREQUEST--':
+				
+				if data == '---CLOSE CONNECTION---':
+					socket.close()
+					sys.exit()
+					break
+
+				if data == '---HEARTBEAT---':
+					socket.sendto(rsa.encrypt('---HEARTBEAT---'.encode('utf-8'), KEY))
+				if data != '---RSA KEYS REQUEST---':
 					print(data)
+
 		
 				time.sleep(.5)
 		except:
 			pass
-	
+
 socket = socket.socket(socket.AF_INET ,socket.SOCK_DGRAM)
 socket.bind((HOST,PORT))
 socket.setblocking(0)
 
-socket.sendto('---KEYREQUEST---'.encode('utf-8'), server)
-
+socket.sendto('---RSA KEY REQUEST---'.encode('utf-8'), server)
+iteration = 0
 while key_recieved == False:
 	try:
-		data, addr = socket.recvfrom(1024)
+		data, addr = socket.recvfrom(2048)
 		key_recieved = True
 	except Exception as e:
-		time.sleep(.2)
-		print('Recieving session key...')
+		time.sleep(.5)
+		if iteration == 9:
+			iteration = 0
+		suffix = iteration // 2
+		iteration += 1
+		print('Initializing secure connection' + str('.') * suffix, end='\r')
 		pass
-key = data
-f = Fernet(key)
+
+
+print('Secure connection initialized, public key :\n', data.decode('utf-8'))
+print('\n\n')
+global KEY
+KEY = rsa.PublicKey.load_pkcs1(data)
+user = str(input('Login : '))
+if len(list(user)) > 256:
+	print('Too long login (>256 chars)')
+	sys.exit()
+password = str(input('Password : '))
+if len(list(password)) > 256:
+	print('Too long password (>256 chars)')
+	sys.exit()
+hash_object = hashlib.sha256(password.encode())
+password = hash_object.hexdigest()
+payload = rsa.encrypt((json.dumps({'login': user, 'password': password})).encode('utf-8'), KEY)
+
+while authorized == False:
+	try:
+		print('Authorizing...', end='\r')
+		socket.sendto(payload, server)
+		data, addr = socket.recvfrom(2048)
+		if data.decode('utf-8') == '---AUTHORIZATION FAILURE---':
+			print('Authorization failure : Invalid login or password.')
+			socket.close()
+			sys.exit()
+		elif data.decode('utf-8') == '---AUTHORIZED SUCCESS---':
+			print('Welcome back,', user, '.')
+			authorized = True
+			break
+		else:
+			print('Authorization failure : Invalid server response (', data.decode('utf-8') ,')')
+			socket.close()
+			sys.exit()
+	except Exception as e:
+		time.sleep(.5)
+		print('Authorizing...', end='\r')
+		pass
 recieveThread = threading.Thread(target = recieve, args = ("RTrieve", socket))
 recieveThread.start()
 
-alias = str(input('Please, enter your name: '))
-
 while shutdown == False:
 	if join == False:
-		socket.sendto(f.encrypt(("["+alias + "] - Joined the chat ").encode("utf-8")),server)
+		socket.sendto(rsa.encrypt(("---JOIN CHAT---").encode("utf-8"), KEY),server)
 
 		join = True
 	else:
 		try:
-			message = str(input())
+			message = str(input('>'))
 
 			if message != "":
-				socket.sendto(f.encrypt(("["+alias + "] ==> " + message).encode("utf-8")),server)
+				socket.sendto(rsa.encrypt((message).encode("utf-8"), KEY),server)
 
 			time.sleep(.5)
 		except Exception as e:
-			socket.sendto(f.encrypt(("["+alias + "] - Left the chat ").encode("utf-8")),server)
+			socket.sendto(rsa.encrypt(("---LEFT CHAT---").encode("utf-8"), KEY),server)
 			recieveThread.join()
 			socket.close()
 			input('Press enter to exit.')
--- server.py
+++ server.py
@@ -1,53 +1,68 @@
 import socket
 import select
 import time
+import hashlib
+import rsa
+import json
 from datetime import datetime
-############KEYGEN###############
-import base64
-import string
-import random
-import os
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-N = 128
-password_provided = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N)) # This is input in the form of a string
-password = password_provided.encode()
-salt = os.urandom(16)
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=128452,
-    backend=default_backend()
-)
-key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
-############KEYGEN###############
-HOST = '0.0.0.0'
-PORT = 8080
+global CLIENTS
+global USERS
+####################
+print('[Generating RSA keypair...]')
+(PUBKEY, PRIVATEKEY) = rsa.newkeys(2048)
+PUBKEY_STR = PUBKEY.save_pkcs1()
+####################
+#CLIENTS = {
+#	'192.168.0.1': 'OFFLINE | AUTH | ONLINE | NO AUTH | KEY REQUEST'	
+#}
+CLIENTS = {}
+USERS = {
+	'George': '532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25'
+#	'George2': '532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25'
+}
+
+HOST = '127.0.0.1'
+PORT = 8760
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket.bind((HOST, PORT))
 
-clients = []
-print('[Server started]')
+
+print('[Dedicated server started]')
 while True:
 	try:
-		data, addr = socket.recvfrom(1024)
-		if data.decode('utf-8') == '---KEYREQUEST---':
-			socket.sendto(key, addr)
-		if addr not in clients:
-			clients.append(addr)
-		timestamp = str(datetime.now())
-
-		print('[{}] [{}] [{}] {}'.format(timestamp, addr[0], addr[1], data.decode('utf-8')))
+		data, addr = socket.recvfrom(2048)
+		if addr not in CLIENTS or CLIENTS[addr] == 'NO AUTH' or CLIENTS[addr] == 'OFFLINE' or CLIENTS[addr] == 'KEY REQUEST':
+			CLIENTS[addr] = 'KEY REQUEST'
+			if data.decode('utf-8') == '---RSA KEY REQUEST---':
+				socket.sendto(PUBKEY_STR, addr)
+				CLIENTS[addr] = 'AUTH'
+		elif CLIENTS[addr] == 'AUTH':
+			try:
+				payload = json.loads(rsa.decrypt(data, PRIVATEKEY).decode('utf-8'))
+			except:
+				socket.sendto('---AUTHORIZATION FAILURE---'.encode('utf-8'), addr)
+				CLIENTS[addr] = 'NO AUTH'
+			else:
+				if payload['login'] in USERS.keys():
+					if USERS[payload['login']] == payload['password']:
+						socket.sendto('---AUTHORIZED SUCCESS---'.encode('utf-8'), addr)
+						CLIENTS[addr] = 'ONLINE'
+				else:
+					socket.sendto('---AUTHORIZATION FAILURE---'.encode('utf-8'), addr)
+					CLIENTS[addr] = 'NO AUTH'	
+		else:	
+			print('[{}] [{}:{}] > {}'.format(str(datetime.now()), addr[0], addr[1], rsa.decrypt(data, PRIVATEKEY).decode('utf-8')))
 		time.sleep(.5)
-		for client in clients:
+		for client in CLIENTS.keys():
 			if addr != client:
 				socket.sendto(data, client)
 	except Exception as e:
-		print('[DEBUG]==>', e)
-		print('[Server closed]')
+		print('[{}] [DEBUG] >'.format(str(datetime.now())), e)
+		for client in CLIENTS.keys():
+			if CLIENTS[client] == 'ONLINE':
+				socket.sendto('---CLOSE CONNECTION---'.encode('utf-8'), client)
+		print('[Dedicated server closed]')
 		break
 
 socket.close()
